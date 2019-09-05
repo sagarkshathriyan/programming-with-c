93fce2d4b9a1101139b6448758e2981ee66f8295
sagarkshathriyan <47881061+sagarkshathriyan@users.noreply.github.com>
2019-09-05 15:49:14 +0530

    Add files via upload

--- insert.c
+++ insert.c
@@ -1,31 +1,35 @@
-//insertionsort
+/*Objective:To perform sorting in a given list of numbers either in ascending order using insertion sort
+  Sagar.N
+  18BBTCS107
+  Function used:void main()
+  Date:05/09/2019 */
 #include<stdio.h>
 int main()
 {
-int n,array[1000],c,d,t;
-printf("enter number of elements \n");
-scanf("%d",&n);
-printf("enter %d integers\n",n);
-for(c=0;c<=n-1;c++)
-{
-scanf("%d",&array[c]);
-}
-for(c=1;c<=n-1;c++)
-{
-d=c;
-while(d>0 && array[d-1]>array[d])
-{
-t=array[d];
-array[d]=array[d-1];
-array[d-1]=t;
-d--;
-}
-}
-printf("sorted list in ascending order:\n");
-for(c=0;c<=n-1;c++)
-{
-printf("%d",array[c]);
-}
-return 0;
+	int n,array[1000],c,d,t;
+	printf("enter number of elements \n");
+		scanf("%d",&n);
+	printf("enter %d integers\n",n);
+	for(c=0;c<=n-1;c++)
+		{
+			scanf("%d",&array[c]);
+		}
+	for(c=1;c<=n-1;c++)
+		{
+			d=c;
+			while(d>0 && array[d-1]>array[d])
+		{
+			t=array[d];
+			array[d]=array[d-1];
+			array[d-1]=t;
+			d--;
+		}
+		}
+	printf("sorted list in ascending order:\n");
+	for(c=0;c<=n-1;c++)
+	{
+		printf("%d",array[c]);
+	}
+	return 0;
 }
 
